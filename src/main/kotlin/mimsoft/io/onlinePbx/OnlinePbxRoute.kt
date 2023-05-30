22783aa0bd4310fc46fbb75b483c670f933d80ca
rustami <jurabek.rustamov1999@gmail.com>
2023-05-30 16:25:22 +0500

    online pbx

--- src/main/kotlin/mimsoft/io/onlinePbx/OnlinePbxRoute.kt
+++ src/main/kotlin/mimsoft/io/onlinePbx/OnlinePbxRoute.kt
@@ -5,9 +5,17 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlin.text.get
+import mimsoft.io.config.FORMATTER
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
 
 fun Route.routeOnlinePbx() {
+
+    val olinePbxService = OnlinePbxService
+
     route("/onlinePbx") {
 
         post("hook") {
@@ -18,6 +26,25 @@ fun Route.routeOnlinePbx() {
             val direction = webhook["direction"]
             val caller = webhook["caller"]
             val callee = webhook["callee"]
+
+            olinePbxService.sendWebSocketMessage(PbxHookModel(event, direction, caller, callee))
+            olinePbxService.saveHook(event, direction, caller, callee, webhook.toString())
+        }
+
+        get("callsHistory") {
+            val startStampFrom = call.parameters["startStampFrom"]?.take(10)
+            if (startStampFrom.isNullOrBlank()) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+
+            }
+            val start = SimpleDateFormat("yyyy-MM-dd").parse(startStampFrom).time / 1000
+            val end = start + 86400
+
+            val calls = olinePbxService.callHistories(start, end)
+            if (calls != null) {
+                call.respond(calls)
+            }
         }
     }
 }
