91572527fffcab7f7e3eb411194a0d00a34515af
HaruunZulkifl <harunzulkifl@gmail.com>
2023-05-30 12:35:00 +0500

    commit resolve3

--- src/main/kotlin/mimsoft/io/table/TableDto.kt
+++ src/main/kotlin/mimsoft/io/table/TableDto.kt
@@ -1,9 +1,12 @@
 package mimsoft.io.table
 
+import mimsoft.io.entities.restaurant.RestaurantDto
+
 data class TableDto (
     val id: Long? = null,
     val name: String? = null,
     val roomId: Long? = null,
     val qr: String? = null,
     val restaurantId: Long? = null,
+//    val restaurant : RestaurantDto? = null
 )
