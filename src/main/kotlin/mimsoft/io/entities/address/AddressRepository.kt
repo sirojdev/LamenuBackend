dd1f9b59ea800efa4f9bf8d6c05205c918163ebb
rustami <jurabek.rustamov1999@gmail.com>
2023-05-30 16:35:52 +0500

    resolve conf

---
+++ src/main/kotlin/mimsoft/io/entities/address/AddressRepository.kt
@@ -0,0 +1,9 @@
+package mimsoft.io.entities.address
+
+interface AddressRepository {
+    suspend fun getAll(): List<AddressTable?>
+    suspend fun get(id: Long?): AddressTable?
+    suspend fun add(addressTable: AddressTable?): Long?
+    suspend fun update(addressTable: AddressTable?): Boolean
+    suspend fun delete(id: Long?): Boolean
+}
