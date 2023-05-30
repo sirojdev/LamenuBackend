6b4afea1afef880538bdc26207ecb1cbcd733c03
HaruunZulkifl <harunzulkifl@gmail.com>
2023-05-31 00:32:04 +0500

    Sales merchant

---
+++ src/main/kotlin/mimsoft/io/entities/seles/SalesMerchantDto.kt
@@ -0,0 +1,14 @@
+package mimsoft.io.entities.seles
+
+import mimsoft.io.utils.TextModel
+
+data class MerchantDto(
+    var id: Long? = null,
+    var subdomain: String? = null,
+    var logo: String? = null,
+    var name: TextModel? = null,
+    var phone: String? = null,
+    var password: String? = null,
+    var domain: String? = null,
+    var isActive: Boolean? = null
+)
---
+++ src/main/kotlin/mimsoft/io/entities/seles/SalesMerchantMapper.kt
@@ -0,0 +1,35 @@
+package mimsoft.io.entities.seles
+import mimsoft.io.utils.TextModel
+
+object MerchantMapper {
+    fun toMerchantTable(restaurantDto: MerchantDto?): MerchantTable? {
+        return if (restaurantDto == null) null else MerchantTable(
+            id = restaurantDto.id,
+            nameUz = restaurantDto.name?.uz,
+            nameRu = restaurantDto.name?.ru,
+            nameEng = restaurantDto.name?.eng,
+            logo = restaurantDto.logo,
+            domain = restaurantDto.domain,
+            subdomain = restaurantDto.subdomain,
+            phone = restaurantDto.phone,
+            password = restaurantDto.password
+        )
+    }
+
+    fun toMerchantDto(merchantTable: MerchantTable?): MerchantDto? {
+        return if (merchantTable == null) null
+        else MerchantDto(
+            id = merchantTable.id,
+            name = TextModel(
+                uz = merchantTable.nameUz,
+                ru = merchantTable.nameRu,
+                eng = merchantTable.nameEng
+            ),
+            logo = merchantTable.logo,
+            domain = merchantTable.domain,
+            subdomain = merchantTable.subdomain,
+            phone = merchantTable.phone,
+            password = merchantTable.password
+        )
+    }
+}
