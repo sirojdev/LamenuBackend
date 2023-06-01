1184d897cc69f381f9cd224e81b13d8738dc6a5c
rustami <jurabek.rustamov1999@gmail.com>
2023-06-01 14:30:28 +0500

    resolve

--- src/main/kotlin/mimsoft/io/table/TableService.kt
+++ src/main/kotlin/mimsoft/io/table/TableService.kt
@@ -1,22 +1,24 @@
 package mimsoft.io.table
 
-import mimsoft.io.utils.DBManager
+import mimsoft.io.repository.BaseRepository
+import mimsoft.io.repository.DBManager
 object TableService : TableRepository {
 
+    val repository: BaseRepository = DBManager
     override suspend fun getAll(): List<TableTable?> =
-        DBManager.getData(dataClass = TableTable::class, tableName = TABLE_TABLE_NAME).filterIsInstance<TableTable?>()
+        repository.getData(dataClass = TableTable::class, tableName = TABLE_TABLE_NAME).filterIsInstance<TableTable?>()
 
     override suspend fun get(id: Long?): TableTable?  =
-        DBManager.getData(dataClass = TableTable::class, id = id, tableName = TABLE_TABLE_NAME).firstOrNull() as TableTable?
+        repository.getData(dataClass = TableTable::class, id = id, tableName = TABLE_TABLE_NAME).firstOrNull() as TableTable?
 
     override suspend fun add(tableTable: TableTable?): Long? =
-        DBManager.postData(dataClass = TableTable::class, dataObject = tableTable, tableName = TABLE_TABLE_NAME)
+        repository.postData(dataClass = TableTable::class, dataObject = tableTable, tableName = TABLE_TABLE_NAME)
 
 
     override suspend fun update(tableTable: TableTable?): Boolean =
-        DBManager.updateData(dataClass = TableTable::class, dataObject = tableTable, tableName = TABLE_TABLE_NAME)
+        repository.updateData(dataClass = TableTable::class, dataObject = tableTable, tableName = TABLE_TABLE_NAME)
 
     override suspend fun delete(id: Long?) : Boolean =
-        DBManager.deleteData(tableName = TABLE_TABLE_NAME, whereValue = id)
+        repository.deleteData(tableName = TABLE_TABLE_NAME, whereValue = id)
 
 }
