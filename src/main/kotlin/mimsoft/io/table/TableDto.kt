782b6b728ba2fe15219f314b2797ccab9c785f1f
rustami <jurabek.rustamov1999@gmail.com>
2023-06-01 15:05:39 +0500

    resolve

--- src/main/kotlin/mimsoft/io/table/TableDto.kt
+++
@@ -1,9 +0,0 @@
-package mimsoft.io.table
-
-data class TableDto (
-    val id: Long? = null,
-    val name: String? = null,
-    val roomId: Long? = null,
-    val qr: String? = null,
-    val restaurantId: Long? = null,
-)
