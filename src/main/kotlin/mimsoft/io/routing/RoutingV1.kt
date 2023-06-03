4716202f0141466101daa3032ff472675f222996
jqodiriy <jamshidxon99@gmail.com>
2023-06-04 00:32:05 +0500

    stash

--- src/main/kotlin/mimsoft/io/routing/RoutingV1.kt
+++ src/main/kotlin/mimsoft/io/routing/RoutingV1.kt
@@ -4,7 +4,7 @@ import io.ktor.server.routing.*
 import mimsoft.io.features.client.routeToUser
 import mimsoft.io.routing.staff.routeToStaffApis
 import mimsoft.io.files.routeToFiles
-import mimsoft.io.routing.admin.routeToAdmin
+import mimsoft.io.routing.v1.routeToAdmin
 import mimsoft.io.routing.client.clientRouting
 import mimsoft.io.routing.merchant.routeToMerchant
 
