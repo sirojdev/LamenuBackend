d4a78b0cb6bcab07315622945c2f30ae30b3ba35
unknown <jkadirov>
2023-07-29 11:57:23 +0500

    changes

--- src/main/kotlin/mimsoft/io/Application.kt
+++ src/main/kotlin/mimsoft/io/Application.kt
@@ -10,7 +10,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
 fun main() {
-    embeddedServer(Netty, port = 9000, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 9001, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
