b4b4ca2621dec6d0760d4ee9a8fb8b9138a6ba71
rustami <jurabek.rustamov1999@gmail.com>
2023-08-08 17:29:56 +0500

    sock en

--- src/main/kotlin/mimsoft/io/services/socket/SocketEntity.kt
+++ src/main/kotlin/mimsoft/io/services/socket/SocketEntity.kt
@@ -3,10 +3,8 @@ package mimsoft.io.services.socket
 import io.ktor.websocket.*
 
 data class SocketEntity(
-    val entityId: Long? = null,
-    val entityType: String? = null,
     val phone: String? = null,
     val password: String? = null,
-    val onPBXCode: Int? = null,
     val session : DefaultWebSocketSession? = null,
+    val details: Map<String, *>? = null
 )
