b5249d06f1a830f13385798280ca5cbd5ec0e7d5
Elmurod2203 <saydaliyevelmurod01@gmail.com>
2023-08-13 12:22:45 +0500

    rebase

--- src/main/kotlin/mimsoft/io/waiter/table/RouteWaitersTable.kt
+++ src/main/kotlin/mimsoft/io/waiter/table/RouteWaitersTable.kt
@@ -7,23 +7,53 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import mimsoft.io.features.staff.StaffPrincipal
 import mimsoft.io.waiter.table.repository.WaiterTableRepository
-import okhttp3.internal.wait
 
 fun Route.routeToWaitersTables() {
     val waterTableRepository = WaiterTableRepository
     route("table") {
-        put("table") {
+        put("join") {
             val staffPrincipal = call.principal<StaffPrincipal>()
             val tableId = call.parameters["tableId"]?.toLong()
             val waiterId = staffPrincipal?.staffId
-            val isOPen = waterTableRepository.isOpenTable(tableId)
-            if(!isOPen){
-                call.respond(HttpStatusCode.MethodNotAllowed)
+                val rs = waterTableRepository.joinToWaiter(waiterId, tableId)
+                if(rs!=null){
+                    call.respond(HttpStatusCode.OK,rs)
+                }else{
+                    call.respond(HttpStatusCode.MethodNotAllowed)
+                }
+        }
+        get("active") {
+            val staffPrincipal = call.principal<StaffPrincipal>()
+            val waiterId = staffPrincipal?.staffId
+            val limit = call.parameters["limit"]?.toIntOrNull() ?: 10
+            val offset = call.parameters["offset"]?.toIntOrNull() ?: 0
+            val activeTables = waterTableRepository.getActiveTablesWaiters(waiterId,limit,offset)
+            if (activeTables.data?.isEmpty() == true) {
+                call.respond(HttpStatusCode.NoContent)
             }
-
-            waterTableRepository.joinToWaiter(waiterId,tableId)
-
-
+            call.respond(HttpStatusCode.OK, activeTables)
+        }
+        get("finished") {
+            val staffPrincipal = call.principal<StaffPrincipal>()
+            val waiterId = staffPrincipal?.staffId
+            val limit = call.parameters["limit"]?.toIntOrNull() ?: 10
+            val offset = call.parameters["offset"]?.toIntOrNull() ?: 0
+            val activeTables = waterTableRepository.getFinishedTablesWaiters(waiterId,limit,offset)
+            if (activeTables.data?.isEmpty() == true) {
+                call.respond(HttpStatusCode.NoContent)
+            }
+            call.respond(HttpStatusCode.OK, activeTables)
+        }
+        put("finish"){
+            val staffPrincipal = call.principal<StaffPrincipal>()
+            val tableId = call.parameters["tableId"]?.toLong()
+            val waiterId = staffPrincipal?.staffId
+            val rs = waterTableRepository.finishTable(waiterId, tableId)
+            if(rs){
+                call.respond(HttpStatusCode.OK)
+            }else{
+                call.respond(HttpStatusCode.NotFound)
+            }
         }
     }
 }
--- src/main/kotlin/mimsoft/io/waiter/table/repository/WaiterTableRepository.kt
+++ src/main/kotlin/mimsoft/io/waiter/table/repository/WaiterTableRepository.kt
@@ -2,18 +2,42 @@ package mimsoft.io.waiter.table.repository
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import mimsoft.io.courier.orders.CourierOrderService
+import mimsoft.io.features.room.RoomDto
+import mimsoft.io.features.table.TableDto
 import mimsoft.io.repository.BaseRepository
 import mimsoft.io.repository.DBManager
-import okhttp3.internal.wait
+import mimsoft.io.repository.DataPage
 import java.sql.ResultSet
 
 object WaiterTableRepository {
     val repository: BaseRepository = DBManager
-    suspend fun getActiveTablesWaiters(waiterId: Long?, tableId: Long?): ArrayList<WaiterTableDto> {
-        val query = "select * from $WAITER_TABLE_NAME " +
-                " where table_id = $tableId and waiter_id = $waiterId and  deleted =false and finished_time is null order by join_time "
+    suspend fun getActiveTablesWaiters(waiterId: Long?, limit: Int, offset: Int): DataPage<WaiterTableDto> {
+        val query = """select wt.*, t.name t_name,r.name r_name
+                    from waiter_table wt
+                             inner join tables t on wt.table_id = t.id
+                             inner join room r on r.id = t.room_id
+                    where wt.waiter_id = $waiterId
+                      and wt.deleted = false
+                      and t.deleted = false
+                      and r.deleted = false
+                      and wt.finish_time is null
+                    order by join_time desc 
+                     limit $limit 
+                     offset  $offset"""
+
+
+        val countQuery = """select count(*) count
+                             from waiter_table wt
+                                      inner join tables t on wt.table_id = t.id
+                                      inner join room r on r.id = t.room_id
+                             where wt.waiter_id = $waiterId
+                               and wt.deleted = false
+                               and t.deleted = false
+                               and r.deleted = false
+                               and wt.finish_time is  null """
         val list = ArrayList<WaiterTableDto>()
+        var total: Int? = null
+
         withContext(Dispatchers.IO) {
             repository.connection().use {
                 val rs = it.prepareStatement(query).apply {
@@ -23,10 +47,13 @@ object WaiterTableRepository {
                         getTables(rs)
                     )
                 }
+                val crs = it.prepareStatement(countQuery).executeQuery()
+                if (crs.next()) {
+                    total = crs.getInt("count")
+                }
             }
         }
-        return list
-
+        return DataPage(list, total)
     }
 
     private fun getTables(rs: ResultSet): WaiterTableDto {
@@ -36,13 +63,40 @@ object WaiterTableRepository {
             tableId = rs.getLong("table_id"),
             joinTime = rs.getTimestamp("join_time"),
             finishTime = rs.getTimestamp("finish_time"),
+            table = TableDto(
+                name = rs.getString("t_name"),
+                room = RoomDto(
+                    name = rs.getString("r_name")
+                )
+            )
         )
     }
 
-    suspend fun getFinishedTablesWaiters(waiterId: Long?, tableId: Long?) {
-        val query = "select * from $WAITER_TABLE_NAME " +
-                " where table_id = $tableId and waiter_id = $waiterId and  deleted =false and finished_time is not null order by finished_time "
+    suspend fun getFinishedTablesWaiters(waiterId: Long?, limit: Int, offset: Int): DataPage<WaiterTableDto> {
+        val query = """select wt.*, t.name t_name,r.name r_name
+                       from waiter_table wt
+                                inner join tables t on wt.table_id = t.id
+                                inner join room r on r.id = t.room_id
+                       where wt.waiter_id = $waiterId
+                         and wt.deleted = false
+                         and t.deleted = false
+                         and r.deleted = false
+                         and wt.finish_time is not null
+                       order by join_time desc
+                       limit $limit
+                        offset  $offset """
+
+        val countQuery = """select count(*) count
+                             from waiter_table wt
+                                      inner join tables t on wt.table_id = t.id
+                                      inner join room r on r.id = t.room_id
+                             where wt.waiter_id = $waiterId
+                               and wt.deleted = false
+                               and t.deleted = false
+                               and r.deleted = false
+                               and wt.finish_time is not null """
         val list = ArrayList<WaiterTableDto>()
+        var total: Int? = null
         withContext(Dispatchers.IO) {
             repository.connection().use {
                 val rs = it.prepareStatement(query).apply {
@@ -50,13 +104,19 @@ object WaiterTableRepository {
                 while (rs.next()) {
                     list.add(getTables(rs))
                 }
+                val crs = it.prepareStatement(countQuery).executeQuery()
+                if (crs.next()) {
+                    total = crs.getInt("count")
+                }
             }
         }
+        return DataPage(list, total)
     }
+
 
     suspend fun isOpenTable(tableId: Long?): Boolean {
         val query = "select * from $WAITER_TABLE_NAME " +
-                " where table_id =$tableId and deleted = false and  finished_time is null "
+                " where table_id =$tableId and deleted = false and  finish_time is null "
         var isOpen = true;
         withContext(Dispatchers.IO) {
             repository.connection().use {
@@ -70,13 +130,62 @@ object WaiterTableRepository {
         return isOpen
     }
 
-    suspend fun joinToWaiter(waiterId: Long?, tableId: Long?) {
-        val query = " insert into $WAITER_TABLE_NAME (waiter_id,table_id,join_time) " +
-                " values($waiterId,$tableId,now()) "
+    suspend fun joinToWaiter(waiterId: Long?, tableId: Long?): WaiterTableDto? {
+        val query = "INSERT INTO waiter_table (waiter_id, table_id, join_time)\n" +
+                "SELECT $waiterId,$tableId, now()\n" +
+                "WHERE\n" +
+                "    EXISTS (SELECT 1 FROM staff WHERE id = $waiterId)\n" +
+                "  AND EXISTS (SELECT 1 FROM tables WHERE id = $tableId)\n" +
+                "  AND NOT EXISTS (\n" +
+                "    SELECT 1 FROM waiter_table\n" +
+                "    WHERE  table_id = $tableId AND finish_time is null\n" +
+                "    )\n" +
+                "ON CONFLICT DO NOTHING;"
+
+        val getTableQuery = """select wt.*, t.name t_name,r.name r_name
+                    from waiter_table wt
+                             inner join tables t on wt.table_id = t.id
+                             inner join room r on r.id = t.room_id
+                    where wt.waiter_id = $waiterId 
+                    and wt.table_id = $tableId
+                      and wt.deleted = false
+                      and t.deleted = false
+                      and r.deleted = false
+                      and wt.finish_time is null
+                    order by join_time desc 
+                  """
+
+        var dto: WaiterTableDto? = null
         withContext(Dispatchers.IO) {
             repository.connection().use {
-                it.prepareStatement(query).executeUpdate()
+                val rs = it.prepareStatement(query).apply {
+                }.executeUpdate()
+                if (rs == 1) {
+                    val tableRs = it.prepareStatement(getTableQuery).apply {
+                    }.executeQuery()
+                    if (tableRs.next()) {
+                        dto = getTables(tableRs)
+                    }
+                }
             }
         }
+        return dto
+    }
+
+    suspend fun finishTable(waiterId: Long?, tableId: Long?): Boolean {
+        val query = "update  $WAITER_TABLE_NAME " +
+                " set finish_time = now() " +
+                " where waiter_id = $waiterId  and table_id = $tableId and finish_time is null and deleted = false"
+        var result = true;
+        withContext(Dispatchers.IO) {
+            repository.connection().use {
+                val rs = it.prepareStatement(query).apply {
+                }.executeUpdate()
+                if (rs != 1) {
+                    result = false;
+                }
+            }
+        }
+        return result
     }
 }
