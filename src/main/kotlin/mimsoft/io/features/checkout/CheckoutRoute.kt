8f08adfacd3c0011f221f434f701cf25b5096e79
rustami <jurabek.rustamov1999@gmail.com>
2023-08-14 16:46:28 +0500

    order fix

--- src/main/kotlin/mimsoft/io/features/checkout/CheckoutRoute.kt
+++ src/main/kotlin/mimsoft/io/features/checkout/CheckoutRoute.kt
@@ -7,10 +7,14 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import mimsoft.io.client.user.UserPrincipal
 import mimsoft.io.features.order.OrderModel
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 fun Route.routeToCheckout() {
+    val log: Logger = LoggerFactory.getLogger("CheckoutRoute")
     post("order/checkout") {
         val dto = call.receive<OrderModel>()
+        log.info("CheckoutRoute: $dto")
         val response = CheckoutService.calculate(dto = dto)
         call.respond(response)
     }
