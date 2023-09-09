da43a33378ae51d7188c63e8d9cc4b6e83a750b2
abdulhamid <abdulhamidtuqsonov@gmail.com>
2023-09-09 20:10:59 +0500

    cashback, category

---
+++ src/test/kotlin/mimsoft/io/features/cashback/CashbackServiceTest.kt
@@ -0,0 +1,28 @@
+package mimsoft.io.features.cashback
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CashbackServiceTest {
+
+    @Test
+    fun getAll() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun get() {
+    }
+}
---
+++ src/test/kotlin/mimsoft/io/features/category/repository/CategoryRepositoryImplTest.kt
@@ -0,0 +1,40 @@
+package mimsoft.io.features.category.repository
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CategoryRepositoryImplTest {
+
+    @Test
+    fun getAllByClient() {
+    }
+
+    @Test
+    fun getCategoryForClientById() {
+    }
+
+    @Test
+    fun getAll() {
+    }
+
+    @Test
+    fun get() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun getCategoryByName() {
+    }
+}
