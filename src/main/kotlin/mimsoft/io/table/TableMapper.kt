209525e5ab3dae73acd8d6bc5e1eb9b8f2680e98
Haruun <harunzulkifl@gmail.com>
2023-09-21 11:39:22 +0500

    resolve4

---
+++ src/main/kotlin/mimsoft/io/table/TableMapper.kt
@@ -0,0 +1,25 @@
+package mimsoft.io.table
+
+object TableMapper {
+    fun toTableTable(tableDto: TableDto?): TableTable? {
+        return if (tableDto == null) null
+        else TableTable(
+            id = tableDto.id,
+            name = tableDto.name,
+            roomId = tableDto.roomId,
+            qr = tableDto.qr,
+            restaurantId = tableDto.restaurantId
+        )
+    }
+    fun toTableDto(tableTable: TableTable?):TableDto? {
+        return if(tableTable == null) null
+        else TableDto(
+            id = tableTable.id,
+            name = tableTable.name,
+            roomId = tableTable.roomId,
+            qr = tableTable.qr,
+            restaurantId = tableTable.restaurantId
+        )
+    }
+
+}
