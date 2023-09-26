21afaa50f722aa32f4b07d0065c00ef2ffaa1429
Saydaliyev-Elmurod <saydaliyevelmurod01@gmail.com>
2023-09-26 19:16:45 +0500

    uzum call-back

--- src/main/kotlin/mimsoft/io/integrate/uzum/UzumRoute.kt
+++ src/main/kotlin/mimsoft/io/integrate/uzum/UzumRoute.kt
@@ -32,23 +32,50 @@ fun Route.routeToUzum() {
                 call.respond(HttpStatusCode.MethodNotAllowed)
             }
             if (uzumOrder?.orderId.toString() != callBack.orderNumber) {
-                call.respond(HttpStatusCode.NoContent, "merchant order id not found")
+                call.respond(HttpStatusCode.NoContent, "merchant order id not found or status not equal")
             }
             when (callBack.operationType) {
                 UzumOperationType.AUTHORIZE -> {
-                    UzumService.authorize(callBack)
+                    if (uzumOrder?.operationType!=UzumOperationType.TO_REGISTER){
+                        call.respond(HttpStatusCode.NoContent,"this transaction not found or status not equal")
+                    }else{
+                        UzumRepository.updateOperationType(uzumOrder.uzumOrderId,UzumOperationType.AUTHORIZE)
+//                        UzumService.authorizeTransaction(callBack)
+                        call.respond(HttpStatusCode.OK)
+                    }
                 }
 
                 UzumOperationType.COMPLETE -> {
-                    UzumService.completeTransaction(callBack)
+                    if (uzumOrder?.operationType!=UzumOperationType.TO_REGISTER){
+                        call.respond(HttpStatusCode.NoContent,"this transaction not found or status not equal")
+                    }else{
+                        UzumRepository.updateOperationType(uzumOrder.uzumOrderId,UzumOperationType.AUTHORIZE)
+//                        UzumService.authorizeTransaction(callBack)
+                        call.respond(HttpStatusCode.OK)
+                    }
+//                    UzumService.completeTransaction(callBack)
                 }
 
                 UzumOperationType.REFUND -> {
-                    UzumService.refund(callBack)
+                    if (uzumOrder?.operationType!=UzumOperationType.COMPLETE){
+                        call.respond(HttpStatusCode.NoContent,"this transaction not found or status not equal")
+                    }else{
+                        UzumRepository.updateOperationType(uzumOrder.uzumOrderId,UzumOperationType.REFUND)
+//                        UzumService.authorizeTransaction(callBack)
+                        call.respond(HttpStatusCode.OK)
+                    }
+//                    UzumService.refundTransaction(callBack)
                 }
 
                 UzumOperationType.REVERSE -> {
-                    UzumService.reverse(callBack)
+                    if (uzumOrder?.operationType!=UzumOperationType.AUTHORIZE){
+                        call.respond(HttpStatusCode.NoContent,"this transaction not found or status not equal")
+                    }else{
+                        UzumRepository.updateOperationType(uzumOrder.uzumOrderId,UzumOperationType.AUTHORIZE)
+//                        UzumService.authorizeTransaction(callBack)
+                        call.respond(HttpStatusCode.OK)
+                    }
+//                    UzumService.reverseTransaction(callBack)
                 }
 
                 else -> {}
@@ -56,8 +83,20 @@ fun Route.routeToUzum() {
         }
         post("event-call-back") {
             val callBack = call.receive<UzumEventCallBack>()
-
-
+        }
+        post("refund") {
+            val refund = call.receive<UzumRefund>()
+            if (refund.orderId == null || refund.amount == null) {
+                call.respond(HttpStatusCode.BadRequest, "orderId or amount is null ")
+            }
+            UzumService.refund(refund)
+        }
+        post("reverse") {
+            val reverse = call.receive<UzumRefund>()
+            if (reverse == null) {
+                call.respond(HttpStatusCode.BadRequest, "orderId or amount is null ")
+            }
+            UzumService.reverse(reverse)
         }
     }
 
--- src/main/kotlin/mimsoft/io/integrate/uzum/module/UzumRepository.kt
+++ src/main/kotlin/mimsoft/io/integrate/uzum/module/UzumRepository.kt
@@ -2,19 +2,25 @@ package mimsoft.io.integrate.uzum.module
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import mimsoft.io.integrate.payme.models.OrderTransaction
 import mimsoft.io.repository.BaseRepository
 import mimsoft.io.repository.DBManager
 
 object UzumRepository {
     private val repository: BaseRepository = DBManager
-    suspend fun saveTransaction(uzumResponse: UzumRegisterResponse, orderId: Long?) {
+    suspend fun saveTransaction(
+        uzumResponse: UzumRegisterResponse,
+        orderId: Long?,
+        totalPrice: Long?,
+        operationType: UzumOperationType
+    ) {
         val query =
-            "insert into $UZUM_PAYMENT_TABLE (order_id, uzum_order_id,price,created_date) values ($orderId,$uzumResponse.order_id,0,now())"
+            "insert into $UZUM_PAYMENT_TABLE (order_id, uzum_order_id,price,created_date,operation_type) values ($orderId,?,$totalPrice,now(),?)"
         println("insert transaction uzum")
         return withContext(Dispatchers.IO) {
             repository.connection().use { connection ->
                 val rs = connection.prepareStatement(query).apply {
+                    setString(1, uzumResponse.result?.orderId)
+                    setString(2, operationType.name)
                     this.closeOnCompletion()
                 }.executeUpdate()
             }
@@ -39,11 +45,28 @@ object UzumRepository {
                         uzumOrderId = rs.getString("uzum_order_id"),
                         createdDate = rs.getTimestamp("created_date"),
                         updatedDate = rs.getTimestamp("updated_date"),
-                        price = rs.getLong("price")
+                        price = rs.getLong("price"),
+                        operationType = UzumOperationType.valueOf(rs.getString("operation_type"))
                     )
                 }
             }
         }
         return result
+    }
+
+    suspend fun updateOperationType(uzumOrderId: String?, operationType: UzumOperationType) {
+        val query =
+            "update $UZUM_PAYMENT_TABLE set operation_type= ? where uzum_order_id = ? "
+        println("get transaction uzum")
+        var result: UzumPaymentTable? = null
+        withContext(Dispatchers.IO) {
+            repository.connection().use { connection ->
+                val rs = connection.prepareStatement(query).apply {
+                    setString(1, operationType.name)
+                    setString(2, uzumOrderId)
+                    this.closeOnCompletion()
+                }.executeUpdate()
+            }
+        }
     }
 }
